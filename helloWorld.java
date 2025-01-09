0b7c78baa502c2ee1e023c188976e6f692361bc8
kimgrossteacher <148360456+kimgrossteacher@users.noreply.github.com>
2025-01-09 08:43:02 -0700

    Update helloWorld.java

--- helloWorld.java
+++ helloWorld.java
@@ -2,6 +2,8 @@ public class helloWorld {
 
     public static void main(String[] args){
         System.out.println("hello world");
+        System.out.println("I am a teacher at IHLA");
+        System.out.println("my email address is kim.gross@malad.us");
     }
     
 }
